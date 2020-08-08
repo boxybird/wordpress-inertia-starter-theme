aa71d001a54c85a7ba166104b3c5da259880fcb7
boxybird <andrew.rhyand@gmail.com>
2020-08-08 08:31:22 -0700

    fix formatting

--- src/Transformers/PostTransformer.php
+++ src/Transformers/PostTransformer.php
@@ -19,25 +19,27 @@ class PostTransformer
     protected static function posts(): array
     {
         return collect(self::$wp_query->posts)
-        ->map(function ($post) {
-            return [
-                'id'      => $post->ID,
-                'title'   => get_the_title($post->ID),
-                'content' => apply_filters('the_content', get_the_content(null, false, $post->ID)),
-                'link'    => get_the_permalink($post->ID),
-                'excerpt' => wp_trim_words(get_the_excerpt($post->ID), 50),
-                'image'   => [
-                    'sizes' => [
-                        'full' => get_the_post_thumbnail_url($post->ID),
+            ->map(function ($post) {
+                return [
+                    'id'      => $post->ID,
+                    'title'   => get_the_title($post->ID),
+                    'content' => apply_filters('the_content', get_the_content(null, false, $post->ID)),
+                    'link'    => get_the_permalink($post->ID),
+                    'excerpt' => wp_trim_words(get_the_excerpt($post->ID), 50),
+                    'image'   => [
+                        'sizes' => [
+                            'full' => get_the_post_thumbnail_url($post->ID),
+                        ],
+                        'meta' => [
+                            'alt' => get_post_meta(
+                                get_post_thumbnail_id($post->ID),
+                                '_wp_attachment_image_alt',
+                                true
+                            ),
+                        ],
                     ],
-                    'meta' => [
-                        'alt' => get_post_meta(
-                            get_post_thumbnail_id($post->ID), '_wp_attachment_image_alt', true
-                        ),
-                    ]
-                ],
-            ];
-        })
-        ->toArray();
+                ];
+            })
+            ->toArray();
     }
 }
